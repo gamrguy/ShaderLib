a4c54778c547c100187303e8abe79108ec02629f
gamrguy <colmandavenport@gmail.com>
2016-09-02 23:15:26 -0400

    Removed unnecessary ItemCustomizer code

--- ShaderLibMod.cs
+++ ShaderLibMod.cs
@@ -4,17 +4,15 @@ using Microsoft.Xna.Framework.Input;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
-using TerraUI;
-using TerraUI.Utilities;
 
-namespace ItemCustomizer
+namespace ShaderLib
 {
-	public class CustomizerMod : Mod
+	public class ShaderLibMod : Mod
 	{
 		//public static UI.Windows.CustomizerUI gui;
 		public static bool guiOn;
 
-		public CustomizerMod()
+		public ShaderLibMod()
 		{
 			Properties = new ModProperties()
 			{
@@ -26,34 +24,7 @@ namespace ItemCustomizer
 
 		public override void Load()
 		{
-			UIUtils.Mod = this;
-			UIUtils.Subdirectory = "TerraUI/";
-		}
-
-		public override void AddRecipeGroups()
-		{
-			Item item = new Item();
-			item.SetDefaults(ItemID.StrangePlant1);
-			RecipeGroup group = new RecipeGroup(() => Lang.misc[37] + " " + item.name, new int[]
-				{
-					ItemID.StrangePlant1,
-					ItemID.StrangePlant2,
-					ItemID.StrangePlant3,
-					ItemID.StrangePlant4
-				});
-			RecipeGroup.RegisterGroup("ItemCustomizer:AnyStrangePlant", group);
-		}
-
-		public override void PostDrawInterface(SpriteBatch spriteBatch)
-        {
-			UIPlayer player = Main.player[Main.myPlayer].GetModPlayer<UIPlayer>(this);
-
-			if(!Main.playerInventory)
-				guiOn = false;
-
-			if(guiOn) {
-				player.DrawUI(spriteBatch);
-			}
+			
 		}
 	}
 }
